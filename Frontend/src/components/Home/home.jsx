15666647798d74486cb92ee3a7fede1645a7928f
Yelnats <s.jeanjr@gmail.com>
2024-06-20 15:38:39 -0400

    updated type on home

--- Frontend/src/components/Home/home.jsx
+++ Frontend/src/components/Home/home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
         <>
             <div className="container mt-4 text-center">
                 <h1>Just testing out more React!</h1>
-                <p>This project began with me wanting to showcase a collection of Funko Pops but it ended with me just experimenting with differnt parts of React and Bootstrap with the addition of a backend server!</p>
+                <p>This project began with me wanting to showcase a collection of Funko Pops but it ended with me just experimenting with different parts of React, React Router, and Bootstrap with the addition of a backend server all hosted on AWS!</p>
                 <p>Welcome to my Play House</p>
             </div>
         </>
