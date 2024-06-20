72d7e70c35af3c91f03e895039bcd75d6b84ef01
Yelnats <s.jeanjr@gmail.com>
2024-06-20 03:20:04 -0400

    dropped collaspe from Nav Bar as there is no burger menu to take the menu place

--- Frontend/src/Navbar/Nav.jsx
+++ Frontend/src/Navbar/Nav.jsx
@@ -18,7 +18,7 @@ const Nav = () => {
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container-fluid">
                 <Link className="navbar-brand" to='/'>Random Collection</Link>
-                <div className="collapse navbar-collapse" id="navbarNav">
+                <div className="navbarNav">
                     <ul className="navbar-nav">
                         <li className="nav-item">
                             <Link className="nav-link" to='/'>Home</Link>
